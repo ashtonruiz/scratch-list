5df33abf962fb99819f608dc062dfd283a03fa54
Ashton Ruiz <ashton.ruiz@yahoo.com>
2022-11-14 15:45:55 -0800

    I added my two functions for my fetch.utils file.

--- fetch.utils.js
+++ fetch.utils.js
@@ -3,3 +3,13 @@ const SUPABASE_KEY =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnaXhobWxzaGl0c2trZW13eWFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDgwODYsImV4cCI6MTk4MzY4NDA4Nn0.nMjJ-vp1PSZuD_oT9AQGKADmPu3OCZp9Uf4n2XbaBjQ';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getFilms() {
+    const response = await client.from('films').select();
+    return response.data;
+}
+
+export async function getFilm(id) {
+    const response = await client.from('films').select().match({ id: id }).single();
+    return response.data;
+}
