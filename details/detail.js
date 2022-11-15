d4596cbd0288b468940bc58565260d1b4c13b00b
Ashton Ruiz <ashton.ruiz@yahoo.com>
2022-11-14 16:35:55 -0800

    finished coding out detail.js. Now moving to app.js because it currently only has imports and that's it

--- details/detail.js
+++ details/detail.js
@@ -2,3 +2,11 @@ import { getFilm } from '../fetch.utils.js';
 import { renderFilmDetail } from '../render.utils.js';
 
 const filmDetailContainer = document.getElementById('film-detail-container');
+
+window.addEventListener('load', async () => {
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get('id');
+    const film = await getFilm(id);
+    const filmDetailEl = renderFilmDetail(film);
+    filmDetailContainer.append(filmDetailEl);
+});
